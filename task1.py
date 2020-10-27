3f0a96a871c53885ef547eca07f1bfa16641ac52
antijun <68279777+antijun@users.noreply.github.com>
2020-10-27 11:28:10 -0700

    update

--- task1.py
+++ task1.py
@@ -22,6 +22,7 @@ getGrades(list) - Receives a list of grades and stores that in the class propert
 constructor     - should require the student name, studentNumber and grade (in that order)
 """
 
+
 class student:
 
     name = ""
@@ -32,26 +33,26 @@ class student:
 
     # properties should be listed first
 
-    def __init__(self, name, studentNumber, grade, courses, gradeList): # You will need to create your own input parameters for all methods
+    # You will need to create your own input parameters for all methods
+    def __init__(self, name, studentNumber, grade, courses = [], gradeList = []):
         self.name = name
         self.studentNumber = studentNumber
         self.grade = grade
         self.courses = courses
         self.gradeList = gradeList
-        
 
     def __del__(self):
-        
+        pass
 
     def average(self):
-        gAverage = sums(self.gradeList) / len(self.gradeList)
-        print gAverage
-        
+        gAverage = sum(self.gradeList) / len(self.gradeList)
+        print(gAverage)
 
     def getHonorRoll(self):
         self.gradeList.sort()
         self. gradeList.reverse()
-        hAverage = (self.gradeList[0] + self.gradeList[1] + self.gradeList[2] + self.gradeList[3] + self.gradeList[4]) / 5
+        hAverage = (self.gradeList[0] + self.gradeList[1] +
+                    self.gradeList[2] + self.gradeList[3] + self.gradeList[4]) / 5
         if hAverage >= 86:
             return True
         else:
@@ -71,26 +72,26 @@ class student:
     def getCourses(self, clist):
         clist = self.courses
 
-
-    def getGrades(self):
+    def getGrades(self, glist):
+        glist = self.gradeList
 
     def constructor(self):
-
+        pass
 
 
 def main():
     # This contains test data that will be used by the autograder.
     # do not modify this function
 
-    st1 = student("Anita Bath","91334",11)
-    st1.getCourses( ["English","Math","PE","Computers","History","Biology","Japanese"] )
-    st1.getGrades( [91, 94, 87, 99, 82, 100, 73])
+    st1 = student("Anita Bath", "91334", 11)
+    st1.getCourses(["English", "Math", "PE", "Computers",
+                    "History", "Biology", "Japanese"])
+    st1.getGrades([91, 94, 87, 99, 82, 100, 73])
 
-    st2 = student("Joe Lunchbox","12346", 11)
-    st1.getCourses( ["English","Math","Physics","Computers","Geography","Chemistry","French"] )
-    st1.getGrades( [71, 98, 93, 95, 68, 81, 71])
-
-
+    st2 = student("Joe Lunchbox", "12346", 11)
+    st1.getCourses(["English", "Math", "Physics", "Computers",
+                    "Geography", "Chemistry", "French"])
+    st1.getGrades([71, 98, 93, 95, 68, 81, 71])
 
 
 main()
